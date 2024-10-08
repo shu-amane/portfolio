6eb932e9acca8378d4de5f8807977f41818b5b39
amane <shu.buy@gmail.com>
2024-10-08 13:50:47 +0900

    configre image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import { IconContext } from "react-icons"
 import { ImTwitter, ImGithub } from "react-icons/im"
+import ProfileImage from "../../public/images/profile-image.jpg"
 
 function Hero() {
   return(
     <section id="home" className="h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="text-center">
-        <img src="../../images/profile-image.jpg" alt="" className="object-containn h-96 w-96 rounded-full" />
+        <img src={ProfileImage} alt="" className="object-containn h-96 w-96 rounded-full" />
         <h2 className="text-4xl md:text-6xl font-bold">Amane</h2>
         <p className="mt-4 text-lg md:text-2xl">JOB:Engineer</p>
         <div className="flex justify-center items-center mt-4">
